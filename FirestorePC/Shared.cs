e0e373d9654e9b01a391a740038db854ea422a93
Bibon <165363279+bibonuwu@users.noreply.github.com>
2025-11-22 12:42:15 +0500

    0.1

---
+++ FirestorePC/Shared.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+using Google.Cloud.Firestore;
+
+namespace Shared
+{
+    public static class FirestoreProvider
+    {
+        public static FirestoreDb Db { get; }
+
+        static FirestoreProvider()
+        {
+            var baseDir = AppDomain.CurrentDomain.BaseDirectory;
+            var jsonPath = Path.Combine(baseDir, "bibonrat-firebase-adminsdk-fbsvc-4cd5e8496d.json");
+
+            // путь к JSON для Google SDK
+            Environment.SetEnvironmentVariable("GOOGLE_APPLICATION_CREDENTIALS", jsonPath);
+
+            // !!! сюда поставь свой реальный id проекта из Firebase console
+            const string projectId = "bibonrat";
+
+            Db = FirestoreDb.Create(projectId);
+        }
+    }
+}
